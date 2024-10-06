6e0defa8542fad27f5911c6ee76bf014b1900dfd
bằng đoàn <bangdxph40062@fpt.edu.vn>
2024-10-06 14:17:19 +0700

    Bang

--- DATN_FiveStus/src/main/java/com/example/DATN_WebFiveTus/repository/PhuPhiHoaDonRepository.java
+++ DATN_FiveStus/src/main/java/com/example/DATN_WebFiveTus/repository/PhuPhiHoaDonRepository.java
@@ -4,6 +4,11 @@ import com.example.DATN_WebFiveTus.entity.PhuPhiHoaDon;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PhuPhiHoaDonRepository extends JpaRepository<PhuPhiHoaDon,Integer> {
+    List<PhuPhiHoaDon> findByHoaDonChiTiet_Id(int hoaDonChiTietId);
+//    void deleteById(int id);
+
 }
